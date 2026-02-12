209ccb15ba36bc6db9298eafa7a62d74359d2aa3
haakongd <haakongd@tsud.ntnu.no>
2026-02-12 13:42:09 +0100

    test

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -20,7 +20,7 @@ int main(){
         if(floor == 0){
             elevio_motorDirection(DIRN_UP);
         }
-// hei jonas, nå går det unna;
+// hei jonas, nå går det unna; halla
         if(floor == N_FLOORS-1){
             elevio_motorDirection(DIRN_DOWN);
         }
